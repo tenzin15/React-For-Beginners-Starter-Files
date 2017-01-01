079071f6d7968e0ac49ac2e98177d7b46f07a322
Tenzin Chhosphel <tenchhos@gmail.com>
2017-01-01 15:08:45 -0500

    load data on click function

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
   constructor() {
@@ -26,6 +27,11 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -33,7 +39,7 @@ class App extends React.Component {
         <Header tagline='Fresh Seafood Market' />
       </div>
       <Order />
-      <Inventory addFish={this.addFish} />
+      <Inventory addFish={this.addFish} loadSampels={this.loadSampels} />
       </div>
       )
   }
