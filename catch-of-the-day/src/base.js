073d9edbde065426bec36729c1f285c364cdbbc3
Tenzin Chhosphel <tenchhos@gmail.com>
2017-01-01 23:59:22 -0500

    API Placeholder

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,7 +1,7 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-  apiKey: "API_KEY",
+  apiKey: "ENTER_YOUR_API_KEY",
   authDomain: "catch-of-the-day-tenzin-c.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-tenzin-c.firebaseio.com",
 });
